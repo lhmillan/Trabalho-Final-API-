02e16852b061c84915d22fdd0fbbc78cc22fcae4
MadowZada <rl.madow@gmail.com>
2026-05-28 18:42:17 -0300

    Paginação Rodrigo

--- src/main/java/br/com/serratec/trabfinal_api/repository/OSRepository.java
+++ src/main/java/br/com/serratec/trabfinal_api/repository/OSRepository.java
@@ -1,9 +1,12 @@
 package br.com.serratec.trabfinal_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.serratec.trabfinal_api.model.OS;
 
 
 public interface OSRepository extends JpaRepository<OS, Long> {
+    Page<OS> findAll(Pageable pageable);
 }
